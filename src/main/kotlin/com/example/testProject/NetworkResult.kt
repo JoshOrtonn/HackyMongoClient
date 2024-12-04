5e3e29dfe9814e2b226bc17e9fec4e1b9f587666
Josh Orton <joshorton34@hotmail.co.uk>
2024-12-04 15:47:21 +1100

    slight generics cleanuop

--- src/main/kotlin/com/example/testProject/NetworkResult.kt
+++ src/main/kotlin/com/example/testProject/NetworkResult.kt
@@ -1,6 +1,6 @@
 package com.example.testProject
 
 sealed class NetworkResult<out T, out E: Exception>() {
-    data class Success<out T, out E : Exception>(val data: T): NetworkResult<T, E>()
-    data class Error<out T, out E : Exception>(val error: E, val message: String): NetworkResult<T, E>()
+    data class Success<out T>(val data: T): NetworkResult<T, Nothing>()
+    data class Error<out E : Exception>(val error: E, val message: String): NetworkResult<Nothing, E>()
 }
